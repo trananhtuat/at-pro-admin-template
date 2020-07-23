2d220bf207224331d4ff2ab366cba757febae9a4
Tran Anh Tuat <tuattrananh@gmail.com>
2020-07-23 20:07:45 +0700

    grid system

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const primaryColor = '#4834d4'
+const warningColor = '#f0932b'
+const successColor = '#6ab04c'
+const dangerColor = '#eb4d4b'
+
 const themeCookieName = 'theme'
 const themeDark = 'dark'
 const themeLight = 'light'
@@ -77,3 +82,34 @@ function openCloseDropdown(event) {
 		}
 	}
 }
+
+var ctx = document.getElementById('myChart')
+ctx.height = 500
+ctx.width = 500
+var data = {
+	labels: ['January', 'February', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+	datasets: [{
+		fill: false,
+		label: 'Completed',
+		borderColor: successColor,
+		data: [120, 115, 130, 100, 123, 88, 99, 66, 120, 52, 59],
+		borderWidth: 2,
+		lineTension: 0,
+	}, {
+		fill: false,
+		label: 'Issues',
+		borderColor: dangerColor,
+		data: [66, 44, 12, 48, 99, 56, 78, 23, 100, 22, 47],
+		borderWidth: 2,
+		lineTension: 0,
+	}]
+}
+
+var lineChart = new Chart(ctx, {
+	type: 'line',
+	data: data,
+	options: {
+		maintainAspectRatio: false,
+		bezierCurve: false,
+	}
+})
